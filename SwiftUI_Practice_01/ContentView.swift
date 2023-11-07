7398f488ba0b4aa2976d194263d58a4d18598e96
Hyunndy <yoosa3004@naver.com>
2023-11-07 23:15:25 +0900

    @State 변수의 완전 초 기본!!

--- SwiftUI_Practice_01/ContentView.swift
+++ SwiftUI_Practice_01/ContentView.swift
@@ -8,20 +8,49 @@
 import SwiftUI
 import CoreData
 
-struct Data: Identifiable {
-    var id = UUID()
+/*
+ View는 프로토콜이며, 다양한 Modifier를 제공한다.
+ associatedtype Body : View를 가져야하며,
+ 따라서 body: some View안에는 View를 채택하는 무언가 넣어줘야한다.
+ 
+ 따라서 CustomView를 외부에서 생성해서 넣어주는것도 가능하다.
+ */
+struct ContentView: View {
     
-    var name: String
-    var imageName: String = "t"
+    // Name이 변경되면 View를 다시 그린다!
+    @State var name: String = "현디"
+    
+    var body: some View {
+        VStack {
+            Text("안녕하세요! \(name)")
+            
+            Button {
+                name = "태훈!"
+            } label: {
+                Text("click")
+            }
+        }
+    }
+    
+    /*
+     @State State 변수는 왜필요한가?
+     
+     SwiftUI에서 사용하는 View는 Struct이다.
+     mutating이 없기 때문에 안에 요소가 바뀌면, 그 때 마다 새로운 View를 다시 그려줘야한다.
+     
+     근데 언제 다시 그려져야할까? 무엇이 바뀌었을 때? 트리거가 뭐지? 어떤게 바뀌었을 때 View를 reload시켜야하지?
+     -> 그게 바로 State 변수
+     
+     State변수가 바뀌면 iOS는 그림을 새로 그린다.
+     
+     
+     
+     
+     */
 }
 
-struct ContentView: View {
+struct MyView: View {
     var body: some View {
-        // safeArea침범 옵션 대박
-//        Color(.blue).edgesIgnoringSafeArea(.bottom)
-        
-        
-        // clipShape 이런 modifier 대박사건..
         Color(.orange)
             .frame(width: 300, height: 300)
             .clipShape(RoundedRectangle(cornerRadius: 150))
