a554c72515d779e08102ca7c48c7ba16934c278f
Hyunndy <yoosa3004@naver.com>
2023-11-07 22:40:20 +0900

    List를 나눠주는 Section에 대해

--- SwiftUI_Practice_01/ContentView.swift
+++ SwiftUI_Practice_01/ContentView.swift
@@ -18,17 +18,24 @@ struct Data: Identifiable {
 struct ContentView: View {
     var body: some View {
         
-        let names: [Data] = [Data(name: "현지"), Data(name: "태훈"), Data(name: "얍얍")]
+        let names: [Data] = [Data(name: "현지"), Data(name: "태훈"), Data(name: "얍얍"), Data(name: "현지2"), Data(name: "태훈2"), Data(name: "얍얍2"), Data(name: "현지3"), Data(name: "태훈3"), Data(name: "얍얍3"), Data(name: "현지4"), Data(name: "태훈4"), Data(name: "얍얍4")]
         
         // 동적인 리스트
         List(names) { data in
-            HStack {
-                Text(data.name)
-                Image(data.imageName)
-                    .resizable()
-                    .clipped()
-                    .frame(width: 30, height: 30)
-            }
+            
+            Section(content: {
+                HStack {
+                    Text(data.name)
+                    Image(data.imageName)
+                        .resizable()
+                        .clipped()
+                        .frame(width: 30, height: 30)
+                }
+            }, header: {
+                Text("헤더")
+            }, footer: {
+                Text("푸터")
+            })
         }
         
         // 정적인것
