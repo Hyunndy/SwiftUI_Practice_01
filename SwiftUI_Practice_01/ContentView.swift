057d9e7015c326d3fdd1b97b4d34595f7d8055c8
Hyunndy <yoosa3004@naver.com>
2023-11-07 22:54:50 +0900

    Spacer 기본 학습

--- SwiftUI_Practice_01/ContentView.swift
+++ SwiftUI_Practice_01/ContentView.swift
@@ -17,61 +17,24 @@ struct Data: Identifiable {
 
 struct ContentView: View {
     var body: some View {
-        
-        let names: [Data] = [Data(name: "현지"), Data(name: "태훈"), Data(name: "얍얍"), Data(name: "현지2"), Data(name: "태훈2"), Data(name: "얍얍2"), Data(name: "현지3"), Data(name: "태훈3"), Data(name: "얍얍3"), Data(name: "현지4"), Data(name: "태훈4"), Data(name: "얍얍4")]
-        
-        // 동적인 리스트
-        List(names) { data in
-            
-            Section(content: {
-                HStack {
-                    Text(data.name)
-                    Image(data.imageName)
-                        .resizable()
-                        .clipped()
-                        .frame(width: 30, height: 30)
-                }
-            }, header: {
-                Text("헤더")
-            }, footer: {
-                Text("푸터")
-            })
+        VStack {
+            Image(systemName: "bolt")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200)
+            Spacer()
+            Text("Bolt!")
+            Button {
+                print("Blink!")
+            } label: {
+                Text("Hit")
+            }
         }
-        
-        // 정적인것
-//        List {
-//            HStack {
-//                Image("t")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 30, height: 30)
-//                    .clipped()
-//                Text("현디")
-//            }
-//
-//            HStack {
-//                Image("t")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 30, height: 30)
-//                    .clipped()
-//                Text("태훈")
-//            }
-//
-//            HStack {
-//                Image("t")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 30, height: 30)
-//                    .clipped()
-//                Text("얍얍")
-//            }
-//        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
